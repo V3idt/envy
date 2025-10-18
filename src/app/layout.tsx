f43ff577278d8729c9a2c0fdf68eca4a3268a7c5
v3idt <abelasfaw127@gmail.com>
2025-10-19 02:06:32 +0300

    Added the Navbar with desktop and mobileversions

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { ThemeProvider } from "@/components/ThemeProvider";
+import Navbar from "@/components/Navbar";
+import Sidebar from "@/components/Sidebar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,15 +29,29 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en" suppressHydrationWarning>
-        
+
         <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
           <ThemeProvider
-          attribute="class"
-          defaultTheme="dark"
-          enableSystem
-          disableTransitionOnChange
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
           >
-            {children}
+            <div className="min-h-screen">
+              <Navbar />
+              <main className="py-8">
+                <div className="max-w-7xl mx-auto px-4">
+                  <div className="grid grid-cols-1 lg:grid-cols-12 gap-6">
+                    <div className="hidden lg:col-span-3 lg:block">
+                      <Sidebar />
+                    </div>
+                    <div className="lg:col-span-9">
+                      {children}
+                    </div>
+                  </div>
+                </div>
+              </main>
+            </div>
           </ThemeProvider>
         </body>
       </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,8 @@
-import { Button } from "@/components/ui/button";
-import { SignedIn, SignedOut } from "@clerk/nextjs";
-import { SignInButton } from "@clerk/nextjs";
-import { SignUpButton } from "@clerk/nextjs";
-import { UserButton } from "@clerk/nextjs";
-import { ModeToggle } from "@/components/ModeToggle";
-
 
 export default function Home() {
   return (
     <div>
-      <header className="flex justify-end items-center p-4 gap-4 h-16">
-            <SignedOut>
-              <SignInButton mode="modal" >
-                <Button>
-                  Sign in
-                </Button>
-              </SignInButton>
-              <SignUpButton mode="modal">
-                <Button>
-                  Sign Up
-                </Button>
-              </SignUpButton>
-            </SignedOut>
-            <SignedIn>
-              <UserButton />
-            </SignedIn>
-            <ModeToggle />
-          </header>
+      Homepage Content
     </div>
   );
 }
